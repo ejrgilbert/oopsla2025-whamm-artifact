e665090a767d7f736966843d6eebc2641663d3df
evilg <evgilber@andrew.cmu.edu>
2026-01-05 15:59:12 -0500

    Uncomment processing of results

--- src/run/run-exp.py
+++ src/run/run-exp.py
@@ -826,7 +826,7 @@ def runN(cmd, result, has_wizeng_metrics):
         if too_long and i >= RUNS_FOR_LONG - 1:
             break
         errored, timed_out = run_cmd(f"{HYPERFINE_BIN} --runs 1 --export-csv {timing_csv} --output {out}", cmd, True)
-        # too_long |= process_metrics(result, errored, timed_out, out, timing_csv, has_wizeng_metrics)
+        too_long |= process_metrics(result, errored, timed_out, out, timing_csv, has_wizeng_metrics)
     with open(OUTFILE, "a") as f:
         print(result, file=f)
     result.reset()
